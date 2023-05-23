f54b7eeba6120e9adc52ee61ba4337b68d81cad0
fant1q <fant1kcs@yandex.ru>
2023-05-23 23:02:02 +0300

    Initial Commit

---
+++ BMWMtech/BMWMtechApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMWMtechApp.swift
+//  BMWMtech
+//
+//  Created by Денис Штоколов on 23.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BMWMtechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
